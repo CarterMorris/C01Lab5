c65c631083d5f532cf625400012b0ec119eeaa72
CarterMorris <carter.morris@mail.utoronto.ca>
2024-02-13 10:25:48 -0500

    test 1

--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -27,46 +27,78 @@ test("/postNote - Post a note", async () => {
 });
 
 test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+    const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`);
+
+    expect(getAllNotesRes.status).toBe(200);
+    expect(getAllNotesRes.response).toBe([]);
+});
   
-  test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
+test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
+    const title = "NoteTitleTest1";
+    const content = "NoteTitleContent1";
+
+    const title2 = "NoteTitleTest2";
+    const content2 = "NoteTitleContent2";
+
+    const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        title: title,
+        content: content,
+        }),
+    });
+
+    const postNoteRes2 = await fetch(`${SERVER_URL}/postNote`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        title: title2,
+        content: content2,
+        }),
+    });
+
+    const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`);
+
+    expect(getAllNotesRes.status).toBe(200);
+    expect(getAllNotesRes.response.length()).toBe(2);
+});
+
+test("/deleteNote - Delete a note", async () => {
     // Code here
     expect(false).toBe(true);
-  });
-  
-  test("/deleteNote - Delete a note", async () => {
+});
+
+test("/patchNote - Patch with content and title", async () => {
     // Code here
     expect(false).toBe(true);
-  });
-  
-  test("/patchNote - Patch with content and title", async () => {
+});
+
+test("/patchNote - Patch with just title", async () => {
     // Code here
     expect(false).toBe(true);
-  });
-  
-  test("/patchNote - Patch with just title", async () => {
+});
+
+test("/patchNote - Patch with just content", async () => {
     // Code here
     expect(false).toBe(true);
-  });
-  
-  test("/patchNote - Patch with just content", async () => {
+});
+
+test("/deleteAllNotes - Delete one note", async () => {
     // Code here
     expect(false).toBe(true);
-  });
-  
-  test("/deleteAllNotes - Delete one note", async () => {
+});
+
+test("/deleteAllNotes - Delete three notes", async () => {
     // Code here
     expect(false).toBe(true);
-  });
-  
-  test("/deleteAllNotes - Delete three notes", async () => {
+});
+
+test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
     // Code here
     expect(false).toBe(true);
-  });
-  
-  test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+});
