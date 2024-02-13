ad9be56d8de1531aee3aec601e80d10d510aebd1
CarterMorris <carter.morris@mail.utoronto.ca>
2024-02-13 11:00:31 -0500

    this lab is unbelievably bad LLLLLLLLLLL

--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -5,72 +5,84 @@ test("1+2=3, empty array is empty", () => {
 
 const SERVER_URL = "http://localhost:4000";
 
-// test("/postNote - Post a note", async () => {
-//   const title = "NoteTitleTest";
-//   const content = "NoteTitleContent";
-
-//   const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       title: title,
-//       content: content,
-//     }),
-//   });
-
-//   const postNoteBody = await postNoteRes.json();
-
-//   expect(postNoteRes.status).toBe(200);
-//   expect(postNoteBody.response).toBe("Note added succesfully.");
-// });
-
-test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
-    const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`);
-
-    const notes = await getAllNotesRes.json()
-
-    expect(getAllNotesRes.status).toBe(200);
-    expect(notes.response.length).toBe(0);
-});
-  
-test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
-    const title = "NoteTitleTest1";
-    const content = "NoteTitleContent1";
-
-    const title2 = "NoteTitleTest2";
-    const content2 = "NoteTitleContent2";
-
-    const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
-        method: "POST",
+describe('generating note tests', () => {
+    afterEach(async () => {
+        await fetch(`http://localhost:4000/deleteAllNotes`, {
+        method: "DELETE",
         headers: {
-        "Content-Type": "application/json",
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify({
-        title: title,
-        content: content,
-        }),
+      })
+    })
+
+    test("/postNote - Post a note", async () => {
+        const title = "NoteTitleTest";
+        const content = "NoteTitleContent";
+      
+        const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: title,
+            content: content,
+          }),
+        });
+      
+        const postNoteBody = await postNoteRes.json();
+      
+        expect(postNoteRes.status).toBe(200);
+        expect(postNoteBody.response).toBe("Note added succesfully.");
+      });
+
+      test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
+        const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`);
+    
+        const notes = await getAllNotesRes.json()
+    
+        expect(getAllNotesRes.status).toBe(200);
+        expect(notes.response.length).toBe(0);
     });
-
-    const postNoteRes2 = await fetch(`${SERVER_URL}/postNote`, {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-        title: title2,
-        content: content2,
-        }),
+      
+    test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
+        const title = "NoteTitleTest1";
+        const content = "NoteTitleContent1";
+    
+        const title2 = "NoteTitleTest2";
+        const content2 = "NoteTitleContent2";
+    
+        const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            title: title,
+            content: content,
+            }),
+        });
+    
+        const postNoteRes2 = await fetch(`${SERVER_URL}/postNote`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            title: title2,
+            content: content2,
+            }),
+        });
+    
+        const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`);
+    
+        const notes = await getAllNotesRes.json()
+    
+        expect(getAllNotesRes.status).toBe(200);
+        expect(notes.response.length).toBe(2);
     });
+})
 
-    const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`);
-
-    const notes = await getAllNotesRes.json()
-
-    expect(getAllNotesRes.status).toBe(200);
-    expect(notes.response.length).toBe(2);
-});
 
 test("/deleteNote - Delete a note", async () => {
     // Code here
